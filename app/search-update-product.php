a6c1c23eaa47720b73cbee89e2514a1013178618
AldoF9 <66645860+AldoF9@users.noreply.github.com>
2020-06-12 01:14:37 -0500

    Add files via upload

--- app/search-update-product.php
+++ app/search-update-product.php
@@ -1,32 +1,38 @@
 <?php
 ob_start();
-require_once './includes/nav-login.php';
+require_once './includes/nav-login.php'; 
+
+$sql = 'SELECT * FROM productos ORDER BY marca';
+$resultado = mysqli_query($conn, $sql);
+$productos = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+mysqli_free_result($resultado);
+mysqli_close($conn);
 ?>
 
 <main class="principal principal-1">
     <section>
         <div class="container">
             <div class="text-center block-heading padding-titulo-formularios">
-                <h2 class="text-info tamanio-titulo">Modificar productos</h2>
+                <h2 class="text-info tamanio-titulo">Modificar Productos</h2>
             </div>
             <div class="padding-columnas">
                 <div class="row no-gutters">
-                    
+                    <?php foreach ($productos as $producto){ ?>
                         <div class="col-12 col-md-6 col-lg-4 padding-celda">
                             <div class="clean-product-item">
                                 <div class="image">
-                                    <img class="rounded-circle img-fluid d-block mx-auto imagen" src="img/audifonos.jpg">
+                                    <img class="rounded-circle img-fluid d-block mx-auto imagen" src="<?= $base ?>imagenesproductos/<?php echo htmlspecialchars($producto['imagen']); ?>">
                                 </div>
                                 <div class="text-center product-name margen-txt-col">
-                                    <h2 class="text-info tamanio-titulo-col">Memoria USB</h2>
-                                    <p class="tamanio-subtitulo-col">Kingston</p>
+                                    <h2 class="text-info tamanio-titulo-col"><?php echo htmlspecialchars($producto['nombre']); ?></h2>
+                                    <p class="tamanio-subtitulo-col"><?php echo htmlspecialchars($producto['marca']); ?></p>
                                 </div>
                                 <div class="text-center product-name">
-                                    <a class="btn btn-info btn-color" role="button" href="update-product.php">Modificar</a>
+                                    <a class="btn btn-info btn-color" role="button" href="update-product.php?id=<?php echo $producto['id'] ?>">Modificar</a>
                                 </div>
                             </div>
                         </div>
-                    
+                    <?php } ?>
                 </div>
             </div>
         </div>
--- app/update-product.php
+++ app/update-product.php
@@ -1,41 +1,94 @@
-<?php
+<?PHP
 ob_start();
 require_once './includes/nav-login.php';
+
+if (isset($_GET['id']))
+{
+    $id = mysqli_real_escape_string($conn, $_GET['id']);
+    $sql = "SELECT * FROM productos WHERE id = $id";
+    $resultado = mysqli_query($conn, $sql);
+    $productos = mysqli_fetch_assoc($resultado);
+    $imagenproductos = $productos['imagen'];
+    mysqli_free_result($resultado);
+}
+
+if (isset($_POST['submit']) && isset($_GET['id']))
+{
+    $id = mysqli_real_escape_string($conn, $_GET['id']);
+    $nom = mysqli_real_escape_string($conn, $_POST['nombre']);
+    $mar = mysqli_real_escape_string($conn, $_POST['marca']);
+    $descripcion = mysqli_real_escape_string($conn, $_POST['descripcion']);
+    //Guardar la imagen
+    if (isset($_FILES['imagen']) && $_FILES['imagen']['name'] != null)
+    {
+        unlink("imagenesproductos/$imagenAudifonos");
+        $file = $_FILES['imagen'];
+        $filename = $file['name'];
+        $mimetype = $file['type'];
+        if ($mimetype == "image/jpg" || $mimetype == 'image/jpeg' ||
+                $mimetype == 'image/png' || $mimetype == 'image/gif')
+        {
+            if (!is_dir('imagenesproductos'))
+            {
+                mkdir('imagenesproductos', 0777, TRUE);
+            }
+            move_uploaded_file($file['tmp_name'], 'imagenesproductos/' . $filename);
+        }
+        $sql = "UPDATE productos SET nombre = '$nom', marca = '$mar', "
+                . "imagen = '$filename', descripcion = '$descripcion' "
+                . "WHERE id = $id";
+    } else
+    {
+        $sql = "UPDATE productos SET nombre = '$nom', marca = '$mar', "
+                . "descripcion = '$descripcion' "
+                . "WHERE id = $id";
+    }
+    if (mysqli_query($conn, $sql))
+    {
+        header('Location: read-products.php');
+    } else
+    {
+        echo 'Error al actualizar el producto: ' . mysqli_error($conn);
+    }
+}
+
+mysqli_close($conn);
 ?>
 
 <main class="principal principal-1">
     <section>
         <div class="container">
             <div class="text-center block-heading padding-titulo-formularios">
-                <h2 class="text-info tamanio-titulo">Modificar producto</h2>
+                <h2 class="text-info tamanio-titulo">Modificar audífonos</h2>
             </div>
-            <form class="formulario" enctype="multipart/form-data">
+            <form class="formulario" action="update-product.php?id=<?php echo $productos['id'] ?>" method="POST" enctype="multipart/form-data">
                 <div class="form-group">
-                    <label for="">Modelo</label>
-                    <input class="form-control ancho-artista" type="text">
+                    <label for="">Nombre</label>
+                    <input class="form-control ancho-marca" type="text" name="nombre" id="nombre" value="<?php echo htmlspecialchars($productos['nombre']); ?>" autofocus>
                 </div>
                 <div class="form-group">
                     <label for="">Marca</label>
-                    <input class="form-control ancho-marca" type="text">
-                </div>
-                <div>
-                    <label for="">Descripción</label>
-                    <textarea class="form-control altura-desc"></textarea>
+                    <input class="form-control ancho-artista" type="text" name="marca" id="marca" value="<?php echo htmlspecialchars($productos['marca']); ?>" autofocus>
                 </div>
                 <div class="form-group d-flex flex-column">
                     <label for="">Imagen</label>
-                    <input type="file">
+                    <input type="file" name="imagen" id="imagen" autofocus>
                 </div>
                 <div class="form-group d-flex justify-content-center">
-                    <img class="ancho-imagen" src="img/discos.jpg">
+                    <img class="ancho-imagen" id="imagenSalida" src="<?= $base ?>imagenesproductos/<?php echo htmlspecialchars($productos['imagen']); ?>">
                 </div>
+                <div class="form-group">
+                    <label for="">Descripción</label>
+                    <textarea class="form-control altura-desc" name="descripcion" id="descripcion" autofocus><?php echo htmlspecialchars($productos['descripcion']); ?></textarea>
+                </div>
+                
                 <input class="btn btn-info btn-block" type="submit" name="submit" value="Guardar cambios">
             </form>
         </div>
     </section>
 </main>
 
-<?php
+<?php 
 ob_end_flush();
-require_once './includes/footer.php';
-?>
+require_once './includes/footer.php'; 
+?>  
