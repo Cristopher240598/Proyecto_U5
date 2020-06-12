50eadbf3e685fcbf5abc5d4c00d4a8ce0abea873
AldoF9 <66645860+AldoF9@users.noreply.github.com>
2020-06-11 23:13:03 -0500

    Add files via upload

--- app/read-products.php
+++ app/read-products.php
@@ -1,6 +1,12 @@
 <?php
 ob_start();
 require_once './includes/nav-login.php';
+
+$sql = 'SELECT * FROM productos ORDER BY marca';
+$resultado = mysqli_query($conn, $sql);
+$producto = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+mysqli_free_result($resultado);
+mysqli_close($conn);
 ?>
 
 <main class="principal principal-1">
@@ -9,25 +15,25 @@ require_once './includes/nav-login.php';
             <div class="text-center block-heading padding-titulo-formularios">
                 <h2 class="text-info tamanio-titulo">Productos</h2>
             </div>
-            <div class="padding-columnas">
+            <div class="products padding-columnas">
                 <div class="row no-gutters">
-                    
+                    <?php foreach ($producto as $productos){ ?>
                         <div class="col-12 col-md-6 col-lg-4 padding-celda">
                             <div class="clean-product-item">
                                 <div class="image">
-                                    <img class="rounded-circle img-fluid d-block mx-auto imagen" src="img/audifonos.jpg">
+                                    <img class="rounded-circle img-fluid d-block mx-auto imagen" src="<?= $base ?>imagenesproductos/<?php echo htmlspecialchars($productos['imagen']); ?>">
                                 </div>
                                 <div class="text-center margen-txt-col padding-texto-art">
                                     <p class="tamanio-txt">
-                                        ID: 5<br>
-                                        Nombre: Memoria USB<br>
-                                        Marca: Kingston<br>
-                                        Descripción: sdfgf dfhdf dghd ghd gdh dghdgdd dghd dhd  dgfh  fhjf  fghj
+                                        ID: <?php echo htmlspecialchars($productos['id']); ?><br>
+                                        Nombre: <?php echo htmlspecialchars($productos['nombre']); ?><br>
+                                        Marca: <?php echo htmlspecialchars($productos['marca']); ?><br>                                      
+                                        Descripción: <?php echo htmlspecialchars($productos['descripcion']); ?>
                                     </p>
                                 </div>
                             </div>
                         </div>
-                    
+                    <?php } ?>
                 </div>
             </div>
         </div>
@@ -37,4 +43,4 @@ require_once './includes/nav-login.php';
 <?php
 ob_end_flush();
 require_once './includes/footer.php';
-?>
+?>  
