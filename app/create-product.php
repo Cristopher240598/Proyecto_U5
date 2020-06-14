5f2ada6af3c7bd38a3f93570735ce4ee94c766e1
unknown <raulloeo1@gmail.com>
2020-06-14 10:42:45 -0500

    Validación JQuery Create

--- app/create-product.php
+++ app/create-product.php
@@ -44,22 +44,22 @@ if (isset($_POST['submit']))
             <form class="formulario" action="create-product.php" method="POST" enctype="multipart/form-data">
                 <div class="form-group">
                     <label for="">Nombre</label>
-                    <input class="form-control ancho-artista" type="text" name="nombre" id="nombre" autofocus="">
+                    <input class="form-control ancho-artista" type="text" name="nombre" id="nombre" autofocus="" maxlength="200" required>
                 </div>
                 <div class="form-group">
                     <label for="">Marca</label>
-                    <input class="form-control ancho-marca" type="text" name="marca" id="marca" autofocus="">
+                    <input class="form-control ancho-marca" type="text" name="marca" id="marca" autofocus="" maxlength="200" required>
                 </div>
                 <div class="form-group d-flex flex-column">
                     <label for="">Imagen</label>
-                    <input type="file" name="imagen" id="imagen" autofocus>
+                    <input type="file" name="imagen" id="imagen" autofocus accept="image/" required>
                 </div>
                 <div class="form-group d-flex justify-content-center">
                     <img class="ancho-imagen" src="" id="imagenSalida">
                 </div>
                 <div>
                     <label for="">Descripción</label>
-                    <textarea class="form-control altura-desc" name="descripcion" id="descripcion" autofocus=""></textarea>
+                    <textarea class="form-control altura-desc" name="descripcion" id="descripcion" autofocus="" maxlength="16,777,215" required></textarea>
                 </div>
                 
                 <input class="btn btn-info btn-block" type="submit" name="submit" value="Crear">
