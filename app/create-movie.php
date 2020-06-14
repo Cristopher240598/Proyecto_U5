4740e4275ee75fbe9b9b303882d7374b7d44c5da
Eve <Eve@LAPTOP-1R9HN6PJ>
2020-06-14 02:46:50 -0500

    Correccion de errores en CRUD  de Movies

--- app/create-movie.php
+++ app/create-movie.php
@@ -1,6 +1,41 @@
 <?php
 ob_start();
 require_once './includes/nav-login.php';
+
+if (isset($_POST['submit']))
+{
+    $titulopeli = mysqli_real_escape_string($conn, $_POST['titulo']);
+    $repartopeli = mysqli_real_escape_string($conn, $_POST['reparto']);
+    $directorpeli = mysqli_real_escape_string($conn, $_POST['director']);
+    $categoriapeli = mysqli_real_escape_string($conn, $_POST['genero']);
+    $descripcionpeli = mysqli_real_escape_string($conn, $_POST['descripcion']);
+    //Guardar la imagen
+    if (isset($_FILES['imagen']))
+    {
+        $archivo = $_FILES['imagen'];
+        $filename = $archivo['name'];
+        $mimetype = $archivo['type'];
+        if ($mimetype == "image/jpg" || $mimetype == 'image/jpeg' ||
+                $mimetype == 'image/png' || $mimetype == 'image/gif')
+        {
+            if (!is_dir('imagenesPeliculas'))
+            {
+                mkdir('imagenesPeliculas', 0777, TRUE);
+            }
+            move_uploaded_file($archivo['tmp_name'], 'imagenesPeliculas/' . $filename);
+        }
+    }
+    $sql = "INSERT INTO peliculas(id_temaPelicula, titulo, reparto , director, imagen, descripcion ) "
+            . "VALUES('$categoriapeli', '$titulopeli', '$repartopeli', '$directorpeli', '$filename', '$descripcion')";
+    if (mysqli_query($conn, $sql))
+    {
+        header('Location: read-movies.php');
+    } else
+    {
+        echo 'Error en la insercción de pelicula: ' . mysqli_error($conn);
+    }
+}
+
 ?>
 
 <main class="principal principal-1">
@@ -9,40 +44,42 @@ require_once './includes/nav-login.php';
             <div class="text-center block-heading padding-titulo-formularios">
                 <h2 class="text-info tamanio-titulo">Crear película</h2>
             </div>
-            <form class="formulario" enctype="multipart/form-data">
+            <form class="formulario" enctype="multipart/form-data" action="create-movie.php" method="POST">
                 <div class="form-group">
-                    <label for="">Título</label>
-                    <input class="form-control" type="text">
+                    <label for="titulo">Título</label>
+                    <input id="titulo" name="titulo" class="form-control" type="text" value="<?php echo htmlspecialchars($titulopeli) ?>" autofocus="autofocus">
                 </div>
                 <div class="form-group">
-                    <label for="">Reparto</label>
-                    <input class="form-control" type="text">
+                    <label for="reparto">Reparto</label>
+                    <input id="reparto" name="reparto" class="form-control" type="text" value="<?php echo htmlspecialchars($repartopeli) ?>">
                 </div>
                 <div class="form-group">
-                    <label for="">Director</label>
-                    <input class="form-control ancho-disquera" type="text">
-                </div>
+                    <label for="director">Director</label>
+                    <input id="director" name="director" class="form-control ancho-disquera" type="text" value="<?php echo htmlspecialchars($directorpeli) ?>">
+               </div>
                 <div class="form-group">
-                    <label for="">Género</label>
-                    <select class="form-control ancho-genero">
-                        <optgroup label="Género">
-                            <option value="1" selected="">Acción</option>
-                        </optgroup>
+                    <label for="genero">Género</label>
+                    <select id="genero" name="genero" class="form-control ancho-genero" value="<?php echo htmlspecialchars($categoriapeli) ?>">
+                        <option value=" ">--Seleccionar--</option>
+                         <?php 
+                         $sql= 'SELECT * FROM temas_peliculas';
+                         $result= mysqli_query($conn, $sql);
+
+                          while($categorias = mysqli_fetch_array($result)) 
+                          echo "<option  value='".$categorias["id"]."'>".$categorias["genero"]."</option>"; 
+
+                             ?>
                     </select>
-                </div>
+               </div>
                 <div class="form-group">
-                    <label for="">Descripción</label>
-                    <textarea class="form-control altura-desc"></textarea>
+                    <label for="descripcion">Descripción</label>
+                    <textarea id="descripcion" name="descripcion" class="form-control altura-desc" ><?php echo htmlspecialchars($descripcionpeli) ?></textarea>
                 </div>
                 <div class="form-group d-flex flex-column">
-                    <label for="">Imagen</label>
-                    <input type="file">
+                    <label for="imagen">Imagen</label>
+                    <input id="imagen" name="imagen" type="file" value="<?php echo htmlspecialchars($filename) ?>">
                 </div>
-                <div class="form-group d-flex justify-content-center">
-                    <img class="ancho-imagen" src="img/discos.jpg">
-                </div>
-                <input class="btn btn-info btn-block" type="submit" name="submit" value="Crear">
-            </form>
+           </form>
         </div>
     </section>
 </main>
--- app/delete-movie.php
+++ app/delete-movie.php
@@ -1,6 +1,30 @@
 <?php
 ob_start();
 require_once './includes/nav-login.php';
+
+$sql = 'SELECT id, titulo, descripcion  FROM peliculas';
+$result = mysqli_query($conn, $sql);
+$peliculas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+mysqli_free_result($result);
+if (isset($_GET['id']))
+{
+    $id = mysqli_real_escape_string($conn, $_GET['id']);
+    $sqlimagen = "SELECT imagen FROM peliculas WHERE id = $id";
+    $res = mysqli_query($conn, $sqlimagen);
+    $img = mysqli_fetch_assoc($res);
+    $imagenpeli = $img['imagen'];
+    unlink("imagenesPeliculas/$imagenpeli");
+    $sql = "DELETE FROM peliculas WHERE id = $id";
+    if (mysqli_query($conn, $sql))
+    {
+        header('Location: delete-movie.php');
+    } else
+    {
+        echo 'Error al eliminar: ' . mysqli_error($conn);
+    }
+    mysqli_free_result($res);
+}
+mysqli_close($conn);
 ?>
 
 <main class="principal principal-1">
@@ -11,21 +35,22 @@ require_once './includes/nav-login.php';
             </div>
             <div class="padding-columnas">
                 <div class="row no-gutters">
-                    
+                    <?php foreach ($peliculas as $peli) { ?>
                         <div class="col-12 col-md-6 col-lg-4 padding-celda">
                             <div class="clean-product-item">
                                 <div class="image">
-                                    <img class="rounded-circle img-fluid d-block mx-auto imagen" src="img/audifonos.jpg">
+                                    <img class="rounded-circle img-fluid d-block mx-auto imagen" src="<?= $base ?>imagenesPeliculas/<?php echo htmlspecialchars($peli['imagen']); ?>">
                                 </div>
                                 <div class="text-center product-name margen-txt-col">
-                                    <h2 class="text-info tamanio-titulo-col">7 Magníficos</h2>
+                                    <h2 class="text-info tamanio-titulo-col"><?php echo htmlspecialchars($peli['titulo']); ?></h2>
+                                     <p><?php echo htmlspecialchars($peli['descripcion']); ?></p>
                                 </div>
                                 <div class="text-center product-name">
-                                    <a class="btn btn-danger btn-color" role="button" href="update-movie.php">Eliminar</a>
+                                    <a class="btn btn-danger btn-color" role="button" href="delete-movie.php?id=<?php echo $peli['id'] ?>">Eliminar</a>
                                 </div>
                             </div>
                         </div>
-                    
+                    <?php } ?>
                 </div>
             </div>
         </div>
