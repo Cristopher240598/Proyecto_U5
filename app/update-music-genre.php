761c2845adae623f1440f5fbe71558585852a998
unknown <raulloeo1@gmail.com>
2020-06-14 22:16:59 -0500

    VALIDACION UPDATE GENEROS

--- app/update-music-genre.php
+++ app/update-music-genre.php
@@ -1,24 +1,58 @@
 <?php
 ob_start();
 require_once './includes/nav-login.php';
+
+if (isset($_GET['id']))
+{
+    $id = mysqli_real_escape_string($conn, $_GET['id']);
+    $sql = "SELECT * FROM temas_discos WHERE id = $id";
+    $result = mysqli_query($conn, $sql);
+    $temas = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
+}
+
+if (isset($_POST['submit']) && isset($_GET['id']))
+{
+    $genero = mysqli_real_escape_string($conn, $_POST['genero']);
+    $sql = "UPDATE temas_discos SET genero = '$genero' WHERE id = $id";
+    if (mysqli_query($conn, $sql))
+    {
+        header('Location: music-genre.php');
+    } else
+    {
+        echo 'Error al actualizar en temas discos, verifique query: ' . mysqli_error($conn);
+    }
+}
+mysqli_close($conn);
 ?>
 
 <main class="principal principal-1">
     <section>
         <div class="container">
             <div class="text-center block-heading padding-titulo-formularios">
-                <h2 class="text-info tamanio-titulo">Modificar género de música</h2>
+                <h2 class="text-info tamanio-titulo">Modificar género de película</h2>
             </div>
-            <form class="formulario">
+            <form class="formulario" method="POST" action="update-music-genre.php?id=<?php echo $temas['id'] ?>">
                 <div class="form-group">
-                    <label for="">Género de música</label>
-                    <input class="form-control" type="text">
+                    <label for="">Género de película</label>
+                    <input class="form-control" type="text" name="genero" id="genero" maxlength="255" onchange="valtexto255('genero')" value="<?php echo htmlspecialchars($temas['genero']); ?>">
                 </div>
                 <input class="btn btn-info btn-block" type="submit" name="submit" value="Guardar cambios">
             </form>
         </div>
     </section>
 </main>
+
+<script>
+  function valtexto255(idinput){
+    x = document.getElementById(idinput).value;
+    if (validartexto200(x)) {
+      document.getElementById(idinput).style.backgroundColor = "#CEF6D8";
+    }else{
+      document.getElementById(idinput).style.backgroundColor = "#F6CECE";
+    }
+  }
+</script>
 
 <?php
 ob_end_flush();
