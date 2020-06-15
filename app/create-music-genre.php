592bf7262eace47fc56a01b14bd9a4af7601f38d
unknown <raulloeo1@gmail.com>
2020-06-14 22:12:32 -0500

    VALIDACION GENERO MUSIC

--- app/create-music-genre.php
+++ app/create-music-genre.php
@@ -1,6 +1,20 @@
 <?php
 ob_start();
 require_once './includes/nav-login.php';
+
+if (isset($_POST['submit']))
+{
+    $genero = mysqli_real_escape_string($conn, $_POST['genero']);
+    $sql = "INSERT INTO temas_discos(genero) "
+            . "VALUES('$genero')";
+    if (mysqli_query($conn, $sql))
+    {
+        header('Location: read-discs.php'); // crea tu vista donde listas los generos y la rediriges ahi
+    } else
+    {
+        echo 'Error al insertar en temas discos, verifique query: ' . mysqli_error($conn);
+    }
+}
 ?>
 
 <main class="principal principal-1">
@@ -9,16 +23,27 @@ require_once './includes/nav-login.php';
             <div class="text-center block-heading padding-titulo-formularios">
                 <h2 class="text-info tamanio-titulo">Crear género de música</h2>
             </div>
-            <form class="formulario">
+            <form class="formulario" method="POST" action="create-music-genre.php">
                 <div class="form-group">
                     <label for="">Género de música</label>
-                    <input class="form-control" type="text">
+                    <input class="form-control" name="genero" id="genero" type="text" maxlength="255" onchange="valtexto255('genero')" required="true">
                 </div>
-                <input class="btn btn-info btn-block" type="submit" name="submit" value="Crear">
+                <input class="btn btn-success btn-block" type="submit" name="submit" value="Crear">
             </form>
         </div>
     </section>
 </main>
+
+<script>
+  function valtexto255(idinput){
+    x = document.getElementById(idinput).value;
+    if (validartexto200(x)) {
+      document.getElementById(idinput).style.backgroundColor = "#CEF6D8";
+    }else{
+      document.getElementById(idinput).style.backgroundColor = "#F6CECE";
+    }
+  }
+</script>
 
 <?php
 ob_end_flush();
